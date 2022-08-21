284340d06155f80d21f2495b2d4404997a5e308b
berkterek <42406263+berkterek@users.noreply.github.com>
2022-08-21 13:14:12 +0300

    feat: cache helper created

---
+++ Panteon2DStrategy/Assets/_GameFolders/Scripts/_Panteon2DStrategyScripts/Concretes/Helpers/CacheHelper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Panteon2DStrategy.Helpers
+{
+    public class CacheHelper
+    {
+        public Vector2 Zero { get; }
+        
+        public CacheHelper()
+        {
+            Zero = Vector2.zero;
+        }
+    }
+}
