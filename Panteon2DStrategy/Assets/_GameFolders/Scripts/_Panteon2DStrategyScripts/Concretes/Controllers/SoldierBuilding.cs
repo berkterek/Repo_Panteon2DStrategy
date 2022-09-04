b73b3f80d0201dc6061bbd102db47a2c4a7e7b64
berkterek <42406263+berkterek@users.noreply.github.com>
2022-09-05 02:45:43 +0300

    refactor: soldier building script updated

--- Panteon2DStrategy/Assets/_GameFolders/Scripts/_Panteon2DStrategyScripts/Concretes/Controllers/SoldierBuilding.cs
+++ Panteon2DStrategy/Assets/_GameFolders/Scripts/_Panteon2DStrategyScripts/Concretes/Controllers/SoldierBuilding.cs
@@ -1,8 +1,6 @@
-﻿using Panteon2DStrategy.Abstracts.Combats;
-using Panteon2DStrategy.Abstracts.Controllers;
+﻿using Panteon2DStrategy.Abstracts.Controllers;
 using Panteon2DStrategy.Helpers;
 using Panteon2DStrategy.Managers;
-using Panteon2DStrategy.Managers.Combats;
 using Panteon2DStrategy.ScriptableObjects;
 using Panteon2DStrategy.ViewModels;
 using Panteon2DStrategyScripts.Helpers;
@@ -23,8 +21,6 @@ namespace Panteon2DStrategy.Controllers
         [BoxGroup("Soldier Info")] [SerializeField]
         Button _createButton;
         
-        public IHealthService HealthManager { get; private set; }
-
         protected override void Awake()
         {
             base.Awake();
